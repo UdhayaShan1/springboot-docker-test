7aa1d9e9f976e8ce3836844dd0a8523c87eb4a5c
UdhayaShan1 <udhaya.shan1@gmail.com>
2024-06-29 10:35:29 +0800

    Edit order dto

--- src/main/java/com/docker3/model/Order.java
+++ src/main/java/com/docker3/model/Order.java
@@ -1,11 +1,15 @@
 package com.docker3.model;
 
 
+import java.time.LocalDate;
+
 import org.hibernate.annotations.Cascade;
 
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import lombok.Getter;
@@ -16,10 +20,16 @@ import lombok.Setter;
 @Getter
 public class Order {
     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
     @Column(nullable = false)
-    private Long orderId;
+    private String orderId;
     @Column(nullable = false)
     private String nameOfOrder;
+    @Column(nullable = false)
+    private LocalDate dateOfOrder;
+    @Column
+    private String descriptionOfOrder;
     @ManyToOne(cascade = CascadeType.ALL)
     private User user;
 }
